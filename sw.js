407ac9b6700ece12383ef3c2153be2e96258cc9e
goudjojeaneudes-lang <goudjojeaneudes@gmail.com>
2026-07-18 14:29:54 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-const CACHE = "toki-v1";
+const CACHE = "toki-v2";
 const ASSETS = [
-  "./",
-  "./index.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
@@ -25,6 +23,12 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
